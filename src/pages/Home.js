e3e1d32a47409f4826a10bfadedcb04617dc8274
divyateja2219 <derugadi@gitam.in>
2025-07-17 11:05:15 +0530

    Added typewriter intro to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
-function Home() {
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+
+export default function Home() {
   return (
-    <div className="text-center text-3xl text-green-600 py-10">
-      Welcome Home 🚀
+    <div style={{ textAlign: "center", padding: "5rem 1rem" }}>
+      <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>Hi, I'm Divya Teja 👋</h1>
+      <h2 style={{ fontSize: "1.5rem", color: "#555" }}>
+        <Typewriter
+          words={['React Developer', 'ML Enthusiast', 'Dreamer']}
+          loop={true}
+          cursor
+          cursorStyle="|"
+          typeSpeed={70}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </h2>
     </div>
   );
 }
-
-export default Home;
