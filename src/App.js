31895efdf90f396d4e6d89915cbe9f4a2e35ff78
divyateja2219 <derugadi@gitam.in>
2025-08-15 23:21:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const DEFAULTS = {
   speed: 0.6, // 0..1
   stroke: 0.5, // 0..1
   hue: 210, // base hue for dynamic palette
-  ccurated: "Aurora", // or null if needed
+  curated: "Aurora", // or null if needed
   seed: 12345,
 };
 
